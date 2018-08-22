977a7f4deb6363924c8d675f76f0f64eeb2362a1
edb-c <edb.cofresi@gmail.com>
2018-08-22 15:45:08 -0400

    Creation

---
+++ lib/marvel_characters_cli_gem_ethel.rb
@@ -0,0 +1,5 @@
+require "marvel_characters_cli_gem_ethel/version"
+
+module MarvelCharactersCliGemEthel
+  # Your code goes here...
+end
---
+++ lib/marvel_characters_cli_gem_ethel/version.rb
@@ -0,0 +1,3 @@
+module MarvelCharactersCliGemEthel
+  VERSION = "0.1.0"
+end
